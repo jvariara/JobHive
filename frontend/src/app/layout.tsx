5b749311c48d34091cfebe6b36d7a6252254e44c
jvariara <jvariara@gmail.com>
2024-02-08 15:07:11 -0500

    added honeycomb pattern background image

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,10 +3,11 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn, constructMetaData } from "@/lib/utils";
 import Navbar from "@/components/Navbar";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = constructMetaData()
+export const metadata = constructMetaData();
 
 export default function RootLayout({
   children,
@@ -17,14 +18,16 @@ export default function RootLayout({
     <html lang="en" className="h-full dark scroll-smooth">
       <body
         className={cn(
-          "relative, h-full font-sans antialiased bg-background",
+          "relative h-full font-sans antialiased bg-background",
           inter.className,
-          "theme-yellow"
+          "bg-[url('/Background.png')] bg-no-repeat bg-left-bottom"
         )}
       >
         <main className="relative flex flex-col min-h-screen">
           <Navbar />
-          <div className="flex-grow flex-1">{children}</div>
+          <div className="flex-grow flex-1">
+            {children}
+          </div>
           {/* Footer */}
         </main>
       </body>

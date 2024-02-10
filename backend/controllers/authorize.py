0ba491c5c47ac28802515197fe9f0cd74990006a
Patrick Muller <patrick.muller.1@stonybrook.edu>
2024-02-10 11:27:19 -0500

    Creating register route
    Trying to create register route. Validating inputs and storing
    the user information into the database is the goal of this route

--- backend/controllers/authorize.py
+++ backend/controllers/authorize.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request
 from models.user import Users
+from password_validator import PasswordValidator
 
 auth_controller = Blueprint('auth_controller', __name__)
 
@@ -7,19 +8,45 @@ auth_controller = Blueprint('auth_controller', __name__)
 def sign_in():
     req = request.json
     res = {
-        'sign-in': '',
+        'status': '',
         'error': ''
     }
     try:
         user = Users.find_by_username(username=req['username'])
         print(user)
         if user and Users.check_password(user, req['password']):
-            res['sign-in'] = 'success'
+            res['status'] = 'success'
         else:
             raise Exception("The username or password credentials are incorrect")
     except Exception as init:
         print("exception")
-        res['sign-in'] = 'error'
+        res['status'] = 'error'
         res['error'] = init.args[0]
         
     return res
+
+@auth_controller.route('/register', methods='POST')
+def register():
+    req = request.json
+    res = {
+        'status': '',
+        'error': '',
+    }
+    try:
+        if not validate_password(req['password']):
+            raise Exception("Your password must contain")
+        pass
+    except Exception:
+        pass
+
+def validate_password(password):  
+    schema = PasswordValidator()
+    schema.min(8) \
+        .max(10) \
+        .has().uppercase() \
+        .has().lowercase() \
+        .has().digits() \
+        .has().no().spaces() \
+        .has().symbols()
+    
+    return schema.validate(password)

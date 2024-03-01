e78c08a3fa83b361c4107d06dbbc4abbb466ceb3
Patrick Muller <patrick.muller.1@stonybrook.edu>
2024-03-01 18:58:35 -0500

    wrote function to get scraped data and add it to the database

--- backend/controllers/middleware.py
+++ backend/controllers/middleware.py
@@ -12,11 +12,11 @@ def token_required(f):
         if 'Authorization' in request.headers:
             token = request.headers['Authorization'].split(" ")[1]
         if not token:
-            return jsonify({'message' : 'Token is missing!'}), 403
+            return jsonify({'message' : 'Token is missing!'})
         try:
             data = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
             current_user = data['sub']
         except:
-            return jsonify({'message': 'Token is invalid!'}), 401
+            return jsonify({'message': 'Token is invalid!'})
         return f(current_user, *args, **kwargs)
     return decorated
